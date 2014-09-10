240abc79a595ceb8370f7bf836513d958b7af478
Android Chromium Automerger <chromium-automerger@android>
2014-09-10 03:33:04 +0000

    Update makefiles after merge of Chromium at a301aef21f9e
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I2c16bf4350d7e1e59500c2824ea004f55869d61b

--- dl/openmax_dl.target.linux-arm.mk
+++ dl/openmax_dl.target.linux-arm.mk
@@ -149,6 +149,7 @@ MY_DEFS_Debug := \
 	'-DDATA_REDUCTION_PROXY_PROBE_URL="http://check.googlezip.net/connect"' \
 	'-DDATA_REDUCTION_PROXY_WARMUP_URL="http://www.gstatic.com/generate_204"' \
 	'-DVIDEO_HOLE=1' \
+	'-DENABLE_LOAD_COMPLETION_HACKS=1' \
 	'-DBIG_FFT_TABLE' \
 	'-DUSE_OPENSSL=1' \
 	'-DUSE_OPENSSL_CERTS=1' \
@@ -255,6 +256,7 @@ MY_DEFS_Release := \
 	'-DDATA_REDUCTION_PROXY_PROBE_URL="http://check.googlezip.net/connect"' \
 	'-DDATA_REDUCTION_PROXY_WARMUP_URL="http://www.gstatic.com/generate_204"' \
 	'-DVIDEO_HOLE=1' \
+	'-DENABLE_LOAD_COMPLETION_HACKS=1' \
 	'-DBIG_FFT_TABLE' \
 	'-DUSE_OPENSSL=1' \
 	'-DUSE_OPENSSL_CERTS=1' \
